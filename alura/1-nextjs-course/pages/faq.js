ad4a61fd1e83b98f9adf1e2affd02b2b265d3b6a
Daniel <danielelielgaio@gmail.com>
2023-02-11 13:12:56 -0300

    - Iniciado o projeto e criada uma navegação SPA basica. - Criado componente Title que é dinamico.

---
+++ alura/1-nextjs-course/pages/faq.js
@@ -0,0 +1,10 @@
+import Link from '../src/components/Link'
+
+export default function FAQPage() {
+  return (
+    <div>
+      <h1>Alura cases - Página de perguntas FAQ</h1>
+      <Link href='/'>Ir para a Home</Link>
+    </div>
+  )
+}
---
+++ alura/1-nextjs-course/pages/index.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import Link from '../src/components/Link'
+
+function Title({ children, as }) {
+  const Tag = as
+  return (
+    <React.Fragment>
+      <Tag>
+        {children}
+      </Tag>
+      <style jsx>{`
+        ${Tag} {
+          color: red;
+          font-family: sans-serif;
+        }
+      `}</style>
+    </React.Fragment>
+  )
+}
+
+export default function HomePage() {
+  return (
+    <div>
+      <Title as='h1'>Alura cases - Home Page</Title>
+      <Link href='/faq'>
+        Ir para o FAQ
+      </Link>
+    </div>
+  )
+}
